ac9eac9b5607b8e3f4cd3a66f6879d8af55db554
Paul W. Gutierrez <86431959+PaulWGutierrez@users.noreply.github.com>
2025-08-24 22:45:52 -0400

    Update Top_International_Football_Goal_Scorers.sql
    Started answering data analysis questions

--- Top_International_Football_Goal_Scorers.sql
+++ Top_International_Football_Goal_Scorers.sql
@@ -17,13 +17,36 @@ CREATE TABLE top_football_goal_scorers
 		
 SELECT * FROM top_football_goal_scorers;
 
--- 1. Which Confederation has the most goal scorers?
--- 2. How many players were from Brazil?
--- 3. How many players were from Argentina?
--- 4. How many players were from Germany?
--- 5. How many players were from France?
--- 6. How many players were in each Confederation?
--- 7. How many players are still active and playing in the present?
--- 8. Out of the 82 players who had the most "Goals per match"?
--- 9.
--- 10.
+-- Data Analysis Questions:
+
+-- Q1. How many players are the top goal scorers with 50 or more goals?
+-- Q2. How many players were in each Confederation?
+-- Q3. How many players were from Argentina?
+-- Q4. How many players were from Germany?
+-- Q5. How many players were from France?
+-- Q6. How many players were from Brazil?
+-- Q7. Which Confederation has the most goal scorers?
+-- Q8. Out of the 82 players who had the most "Goals per match"?
+-- Q9. How many players are still active and playing in the present?
+-- Q10. Which Confederation has the least goal scorers?
+
+-- Data Analysis Exploration and Findings:
+
+-- Q1. How many players are the top goal scorers with 50 or more goals?
+
+SELECT COUNT(DISTINCT player) AS number_of_players from top_football_goal_scorers;
+
+
+-- Q2. How many players were in each Confederation?
+
+SELECT
+	confederation,
+	COUNT(*) as players_per_confederation
+FROM top_football_goal_scorers
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+-- Q3. How many players were from Argentina?
+
+
